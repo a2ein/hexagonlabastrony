042dd3a1cf1f2766bcf1c29a7d15fa00b433a5b7
a2ein <111061082+a2ein@users.noreply.github.com>
2024-12-27 19:42:31 +0600

    Update scripts.js

--- events/scripts.js
+++ events/scripts.js
@@ -1,7 +1,7 @@
 // scripts.js
 document.addEventListener('DOMContentLoaded', () => {
     const events = [
-        { name: ' Eclipse', date: '2024-04-15', description: 'A lunar eclipse will be visible in many parts of the world.', modelUrl: 'path_to_lunar_eclipse_model' },
+        { name: ' Eclipse', date: '2024-04-15', description: 'types of solar eclipses: Total Partial Annular Hybrid  |lunar eclipses: Total Partial Penumbral', modelUrl: 'https://media2.giphy.com/media/FKwnnTY9xZGQoWqcTH/200w.gif?cid=6c09b9529xvbl2xi0gkgkwolxplwc36lcwoj5v5663dyp6d5&ep=v1_videos_search&rid=200w.gif&ct=v' },
         { name: 'Meteor Shower', date: '2024-05-10', description: 'The annual meteor shower will reach its peak tonight.', modelUrl: 'path_to_meteor_shower_model' },
         { name: 'Comet', date: '2024-06-20', description: 'A solar eclipse will be visible from certain regions.', modelUrl: 'path_to_solar_eclipse_model' }, { name: 'Planetary alignment', date: '2024-06-20', description: 'A solar eclipse will be visible from certain regions.', modelUrl: 'path_to_solar_eclipse_model' }, { name: 'Super Luna', date: '2024-06-20', description: 'A solar eclipse will be visible from certain regions.', modelUrl: 'path_to_solar_eclipse_model' },
     ];
@@ -21,6 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
             <h3>${event.name}</h3>
             <p>Date: ${event.date}</p>
             <button onclick="showEventDetails('${event.name}', '${event.date}', '${event.description}', '${event.modelUrl}')">View Details</button>
+            <a href=`{event.modelUrl}`>link here</a>
         `;
         eventsList.appendChild(eventItem);
     });

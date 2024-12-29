eb81e8d8adc2659f4783fab31d48cbe638f4443d
a2ein <111061082+a2ein@users.noreply.github.com>
2024-12-29 11:06:26 +0600

    up to main

--- events/scripts.js
+++ events/scripts.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     const events = [
-        { name: 'Eclipse', date: 'Event Name', description: 'Eclipse: Conditions:The Moon must be in its full phase.Earth, Sun, and Moon must align perfectly, which does/t happen monthly due to the Moons tilted orbit about 5 degrees. Types of Lunar Eclipses:Total Lunar Eclipse: Moon fully enters Earth/s umbra.Partial Lunar Eclipse:/ Only part of the Moon enters Earth/s umbra.Penumbral Eclipse:/ Moon passes through Earths penumbra, causing subtle shading.Eclipse Seasons: Occurs about twice a year when the Moons orbit crosses Earths orbital plane.Blood Moon: Occurs during a total lunar eclipse, where the Moon turns red due to Rayleigh Scattering.Upcoming Blood Moons:March 14, 2025: Not visible from Dhaka. Watch online.September 7-8, 2025: Visible from Dhaka at night.', modelUrl: 'https://www.youtube.com/embed/cxrLRbkOwKs',img:'' },
-        { name: 'Meteor Shower', date: '2024-05-10', description: '', modelUrl: 'path_to_meteor_shower_model' },
-        { name: 'Comet', date: '2024-06-20', description: 'Origin of Meteors Comets: As they near the Sun, they shed gas and dust, leaving a trail that intersects Earths orbit.Asteroids: Debris from asteroid collisions can also cause meteor showers.Meteor Shower Process:Debris Collision: Earth moves through these debris fields, and particles enter the atmosphere at high speeds (25-160 km/s). Friction causes them to heat up, burn, and create bright streaks of light known as meteors.Radiant Point:Constellation Origin: Meteor showers are named after the constellation from which they appear to originate, called the radiant.', modelUrl: 'path_to_comet_model' },
-        { name: 'Planetary Alignment', date: '2024-06-20', description: 'Multiple planets will align.', modelUrl: 'path_to_planetary_alignment_model' },
-        { name: 'Super Luna', date: '2024-06-20', description: 'A super moon will be visible from certain regions.', modelUrl: 'path_to_super_luna_model' },
+        { name: 'Eclipse', date: 'Event Name', description: 'Eclipse: Conditions:The Moon must be in its full phase.Earth, Sun, and Moon must align perfectly, which does/t happen monthly due to the Moons tilted orbit about 5 degrees. Types of Lunar Eclipses:Total Lunar Eclipse: Moon fully enters Earth/s umbra.Partial Lunar Eclipse:/ Only part of the Moon enters Earth/s umbra.Penumbral Eclipse:/ Moon passes through Earths penumbra, causing subtle shading.Eclipse Seasons: Occurs about twice a year when the Moons orbit crosses Earths orbital plane.Blood Moon: Occurs during a total lunar eclipse, where the Moon turns red due to Rayleigh Scattering.Upcoming Blood Moons:March 14, 2025: Not visible from Dhaka. Watch online.September 7-8, 2025: Visible from Dhaka at night.', modelUrl: 'https://youtu.be/VW2xRR75lKE?si=JH5nwtk-G5euplDG',img:'img/ec.jpg' },
+        { name: 'Meteor Shower', date: '2024-05-10', description: '', modelUrl: 'path_to_meteor_shower_model',img:'img/mete.jpg' },
+        { name: 'Comet', date: '2024-06-20', description: 'Origin of Meteors Comets: As they near the Sun, they shed gas and dust, leaving a trail that intersects Earths orbit.Asteroids: Debris from asteroid collisions can also cause meteor showers.Meteor Shower Process:Debris Collision: Earth moves through these debris fields, and particles enter the atmosphere at high speeds (25-160 km/s). Friction causes them to heat up, burn, and create bright streaks of light known as meteors.Radiant Point:Constellation Origin: Meteor showers are named after the constellation from which they appear to originate, called the radiant.', modelUrl: 'https://youtu.be/jSdVT5-4PLA?si=UOXXGxc19JEFeUKT',img:'img/comet.jpg' },
+        { name: 'Planetary Alignment', date: '2024-06-20', description: 'Multiple planets will align.', modelUrl: 'path_to_planetary_alignment_model' ,img:'img/plane.jpg'},
+        { name: 'Super Luna', date: '2024-06-20', description: 'A super moon will be visible from certain regions.', modelUrl: 'https://youtube.com/shorts/mZn0cgw8UWE?si=Mz7dg95J_qhN5jvH',img:'img/superluna.jpg'},
     ];
     
     const eventsList = document.getElementById('events-list');
@@ -20,9 +20,9 @@ document.addEventListener('DOMContentLoaded', () => {
         eventItem.className = 'event';
         eventItem.innerHTML = `
             <h3>${event.name}</h3>
-            <p>Date: ${event.date}</p>
+         <img src="${event.img}"  alt="img">
             <button onclick="showEventDetails('${event.name}', '${event.date}', '${event.description}')">View Details</button>
-            <button onclick="showEventDetails('${event.name}', '${event.date}', '${event.description}', '${event.modelUrl}')">Upcoming</button>
+            <button onclick="showEventDetails('${event.img}', ', '${event.modelUrl}')">Upcoming</button>
             <button onclick="showEventDetails( '${event.description}', '${event.modelUrl}')">Visual Representation</button>
         `;
         eventsList.appendChild(eventItem);
